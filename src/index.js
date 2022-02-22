124fbc7a8f3a804af5c2c4bb3d12903764d3c3da
Pavel Marmurok <pavel.marmurok@gmail.com>
2022-02-22 22:13:43 +0300

    solve part of task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,53 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  if(str.length % 2 !== 0){
+    return false;
+  };
+    
+  let bracketsStr = '';
+  let stack = [];
+  let count = 0;
+  let cur = '';
+  let prev = '';
+  bracketsConfig.forEach(el => {
+    bracketsStr += el.join('');
+   });
+  
+  for(let i = 0; i < str.length; i++){    
+     let curIndex = bracketsStr.indexOf(str[i]);
+     prev = cur; 
+      cur =  bracketsStr[curIndex];  
+         
+     if(curIndex >= 0){      
+      
+      if(bracketsStr[curIndex] === bracketsStr[curIndex + 1]){  
+        
+        
+        if(count % 2 !== 0){
+          curIndex = curIndex + 1;          
+        };       
+             
+        ++count;     
+      };      
+      
+       if(curIndex % 2 !== 0){
+        
+        if(!stack.length){         
+          return false;
+        };        
+
+        let stackPop = stack.pop();    
+          
+        if(stackPop !== bracketsStr[curIndex - 1]){                   
+          return false;
+        };
+               
+      } else {
+        stack.push(str[i]);
+      };
+       
+    }; 
+    
+  }; 
+console.log(!stack.length)
+  return !stack.length;
+};
